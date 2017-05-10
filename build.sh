0bbdddc44c59b6cf76e27eeb1801ddba82843065
Shingo Omura <everpeace@gmail.com>
2017-05-10 20:09:24 +0900

    make build.sh more simple.

--- build.sh
+++ build.sh
@@ -3,6 +3,5 @@
 CONTAINER_NAME=chatwork-notify-resource
 DOCKER_HUB_ORG=everpeace
 
-docker build --no-cache -t $CONTAINER_NAME .
-docker tag -f $CONTAINER_NAME $DOCKER_HUB_ORG/$CONTAINER_NAME
+docker build --no-cache -t $DOCKER_HUB_ORG/$CONTAINER_NAME .
 docker push $DOCKER_HUB_ORG/$CONTAINER_NAME
